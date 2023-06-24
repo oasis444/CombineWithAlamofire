025b8a2bebf97538f3675149e25161f418f5681c
jiwook_han <jiwook.han.dev@gmail.com>
2023-06-24 18:06:26 +0900

    @StateObject

--- CombineWithAlamofire/Views/ContentView.swift
+++ CombineWithAlamofire/Views/ContentView.swift
@@ -9,7 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @ObservedObject var randomUserViewModel = RandomUserViewModel()
+//    @ObservedObject var randomUserViewModel = RandomUserViewModel()
+    @StateObject var randomUserViewModel = RandomUserViewModel()
     
     var body: some View {
         List(randomUserViewModel.randomUsers) { randomUser in
